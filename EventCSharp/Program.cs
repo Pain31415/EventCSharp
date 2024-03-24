fde192e7834a4e943408bafcba1d3c3db9b72b37
Pain31415 <Cherniy412@ukr.net>
2024-03-24 13:02:29 +0200

    Add project files.

---
+++ EventCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EventCSharp
+{
+    public static class StringExtensions
+    {
+        public static int CountSentences(this string str)
+        {
+            int count = 0;
+            foreach (char c in str)
+            {
+                if (c == '.' || c == '?' || c == '!')
+                    count++;
+            }
+            return count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weatherReport = "Сьогодні у Вінниці прекрасна погода: сонячно та тепло, температура становить близько +20°C. Проте, за прогнозом, можливий короткий дощ у другій половині дня. Незважаючи на це, настрій усіх містян відмінний, адже весна вже в повному розпалі, і кожен мріє насолодитися теплим сонячним днем.";
+
+            int sentenceCount = weatherReport.CountSentences();
+            Console.WriteLine($"Кількість речень у рядку: {sentenceCount}");
+        }
+    }
+}
