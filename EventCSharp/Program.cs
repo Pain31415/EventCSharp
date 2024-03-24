d99431078b3ce9855ebf07ce10eeef8cd705a188
Pain31415 <Cherniy412@ukr.net>
2024-03-24 13:02:29 +0200

    Add project files.

---
+++ EventCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EventCSharp
+{
+    public static class StringExtensions
+    {
+        public static int CountVowels(this string str)
+        {
+            int count = 0;
+            foreach (char c in str.ToLower())
+            {
+                if ("aeiou".Contains(c))
+                    count++;
+            }
+            return count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello, how are you today?";
+            int vowelsCount = text.CountVowels();
+            Console.WriteLine($"Кількість голосних у рядку: {vowelsCount}");
+        }
+    }
+}
