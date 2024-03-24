f403f276a6182c5b6d1ea02e26be8be0b8a86346
Pain31415 <Cherniy412@ukr.net>
2024-03-24 13:02:29 +0200

    Add project files.

---
+++ EventCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace EventCSharp
+{
+    public static class StringExtensions
+    {
+        public static int LengthOfLastWord(this string str)
+        {
+            string[] words = str.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length > 0)
+            {
+                string lastWord = words[words.Length - 1];
+                return lastWord.Length;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString1 = "Це тестовий рядок для перевірки довжини останнього слова.";
+            string testString2 = "Привіт!";
+
+            int lengthOfLastWord1 = testString1.LengthOfLastWord();
+            int lengthOfLastWord2 = testString2.LengthOfLastWord();
+
+            Console.WriteLine($"Довжина останнього слова у першому рядку: {lengthOfLastWord1}");
+            Console.WriteLine($"Довжина останнього слова у другому рядку: {lengthOfLastWord2}");
+        }
+    }
+}
