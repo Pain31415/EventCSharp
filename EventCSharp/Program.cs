7b047291c46569396129213b7718dcca322a9960
Pain31415 <Cherniy412@ukr.net>
2024-03-24 13:02:29 +0200

    Add project files.

---
+++ EventCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EventCSharp
+{
+    public static class StringExtensions
+    {
+        public static int WordCount(this string str)
+        {
+            string[] words = str.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return words.Length;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "Це тестовий рядок для перевірки кількості слів у ньому.";
+
+            int wordCount = testString.WordCount();
+
+            Console.WriteLine($"Кількість слів у рядку: {wordCount}");
+        }
+    }
+}
