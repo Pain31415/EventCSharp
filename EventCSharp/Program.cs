2cea0933de21046b63df51d873504f975953a4df
Pain31415 <Cherniy412@ukr.net>
2024-03-24 13:02:29 +0200

    Add project files.

---
+++ EventCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EventCSharp
+{
+    public static class IntExtensions
+    {
+        public static bool IsOdd(this int number)
+        {
+            return number % 2 != 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+
+            Console.WriteLine($"{num1} непарне? {num1.IsOdd()}");
+            Console.WriteLine($"{num2} непарне? {num2.IsOdd()}");
+        }
+    }
+}
