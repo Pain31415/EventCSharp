c1227bc377079898a0837ce332c56a221d799339
Pain31415 <Cherniy412@ukr.net>
2024-03-24 13:02:29 +0200

    Add project files.

---
+++ EventCSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace EventCSharp
+{
+    public static class ArrayExtensions
+    {
+        public static int[] Filter(this int[] array, Func<int, bool> predicate)
+        {
+            var filteredList = new System.Collections.Generic.List<int>();
+
+            foreach (int element in array)
+            {
+                if (predicate(element))
+                {
+                    filteredList.Add(element);
+                }
+            }
+
+            return filteredList.ToArray();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int[] evenNumbers = numbers.Filter(x => x % 2 == 0);
+            Console.WriteLine("Парні числа:");
+            foreach (int num in evenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            int[] oddNumbers = numbers.Filter(x => x % 2 != 0);
+            Console.WriteLine("\nНепарні числа:");
+            foreach (int num in oddNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
