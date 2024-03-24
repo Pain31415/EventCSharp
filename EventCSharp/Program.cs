38beb93ef136c7e9e1e82a4e89797d6b97c8d172
Pain31415 <Cherniy412@ukr.net>
2024-03-24 13:02:29 +0200

    Add project files.

---
+++ EventCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EventCSharp
+{
+    public static class IntExtensions
+    {
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+
+            Console.WriteLine($"{num1} парне? {num1.IsEven()}");
+            Console.WriteLine($"{num2} парне? {num2.IsEven()}");
+        }
+    }
+}
