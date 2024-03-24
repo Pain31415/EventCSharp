04af8679b280a7039cd6742a218c616738fa624f
Pain31415 <Cherniy412@ukr.net>
2024-03-24 13:02:29 +0200

    Add project files.

---
+++ EventCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace EventCSharp
+{
+    public static class IntExtensions
+    {
+        public static bool IsPrime(this int number)
+        {
+            if (number <= 1)
+                return false;
+
+            if (number == 2 || number == 3)
+                return true;
+
+            if (number % 2 == 0 || number % 3 == 0)
+                return false;
+
+            int divisor = 5;
+            while (divisor * divisor <= number)
+            {
+                if (number % divisor == 0 || number % (divisor + 2) == 0)
+                    return false;
+
+                divisor += 6;
+            }
+
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 13;
+            int num2 = 20;
+
+            Console.WriteLine($"{num1} є простим числом? {num1.IsPrime()}");
+            Console.WriteLine($"{num2} є простим числом? {num2.IsPrime()}");
+        }
+    }
+}
